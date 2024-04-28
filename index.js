f9e47734c70a8ba703c534a2cc9f179df8fea03f
kenny-ali-source <kenny.ali@laplateforme.io>
2024-04-28 21:56:41 +0200

    modification logique code pour ajouter localstorage

--- index.js
+++ index.js
@@ -40,27 +40,40 @@ class Upgrade {
       if (upgrade && this.playerStats.gems >= upgrade.cost) {
         this.playerStats.gems -= upgrade.cost;
         upgrade.buyUpgrade();
+        this.saveGame(); 
       } else {
         console.log('Not enough gems or upgrade not found!');
       }
     }
-  }
   
-  class SaveManager {
     saveGame() {
+      const gameData = {
+        upgrades: this.upgrades,
+        playerStats: this.playerStats
+      };
+      localStorage.setItem('gameData', JSON.stringify(gameData));
       console.log('Game saved!');
     }
   
     loadGame() {
-      console.log('Game loaded!');
+      const savedData = localStorage.getItem('gameData');
+      if (savedData) {
+        const gameData = JSON.parse(savedData);
+        this.upgrades = gameData.upgrades;
+        this.playerStats = gameData.playerStats;
+        console.log('Game loaded!');
+      } else {
+        console.log('No saved game found.');
+      }
     }
   }
-  const boutique = new Boutique();
+  
+  const boutique = new Shop();
   boutique.addUpgrade(new Upgrade('Clicker', 10, 1, 0, 'upgrade', [{ multiplier: 2, description: 'Double gem production' }]));
   boutique.addUpgrade(new Upgrade('Pickaxe', 20, 2, 0, 'upgrade', [{ multiplier: 3, description: 'Triple gem production' }]));
-  const saveManager = new SaveManager();
-  boutique.buyUpgrade('Clicker'); 
-  boutique.playerStats.incrementGems(); 
-  saveManager.saveGame(); 
-  saveManager.loadGame();
+  
+  boutique.loadGame(); 
+  
+  boutique.buyUpgrade('Clicker');
+  boutique.playerStats.incrementGems();
   
